c2f79ecfb41f14299f049c892b6dca89ee8edb7f
lisongyan <842166100@qq.com>
2020-04-28 22:07:45 +0800

    重装系统前提交代码

--- gateway/risk-api-server-master/src/main/java/com/cebbank/airm/tech/ComCebbankAirmTechApplication.java
+++ gateway/risk-api-server-master/src/main/java/com/cebbank/airm/tech/ComCebbankAirmTechApplication.java
@@ -4,17 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.server.RequestPredicates;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
-
 @MapperScan("com.cebbank.airm.tech.mapper")
 @SpringBootApplication
 @EnableDiscoveryClient
